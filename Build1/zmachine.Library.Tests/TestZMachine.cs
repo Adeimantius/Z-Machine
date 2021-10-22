023cace96ee7598a72ee37c338eca53e22e1bf30
Jessica Mulein <jessica@mulein.com>
2021-10-22 00:08:46 -0700

    try data test, but still fail

--- Build1/zmachine.Library.Tests/TestZMachine.cs
+++ Build1/zmachine.Library.Tests/TestZMachine.cs
@@ -22,8 +22,9 @@ namespace zmachine.Library.Tests
             }
         }
 
-        [TestMethod]
-        public void TestFirstScreen()
+        [DataTestMethod]
+        [DataRow("ZORK I: The Great Underground Empire\nCopyright(c) 1981, 1982, 1983 Infocom, Inc.All rights reserved.\r\nZORK is a registered trademark of Infocom, Inc.\nRevision 88 / Serial number 840726\r\n\r\nWest of House\r\nYou are standing in an open field west of a white house, with a boarded front door.\r\nThere is a small mailbox here.\r\n\r\n >")]
+        public void TestFirstScreen(string expected)
         {
             StaticIO staticIO = new StaticIO();
             Machine machine = new Machine(
@@ -44,16 +45,7 @@ namespace zmachine.Library.Tests
                 }
             }
             Assert.AreEqual(expected: 386U, actual: machine.InstructionCounter);
-            Assert.AreEqual(expected: @"ZORK I: The Great Underground Empire
-Copyright(c) 1981, 1982, 1983 Infocom, Inc.All rights reserved.
-ZORK is a registered trademark of Infocom, Inc.
-Revision 88 / Serial number 840726
-
-West of House
-You are standing in an open field west of a white house, with a boarded front door.
-There is a small mailbox here.
-
->", actual: staticIO.GetOutput());
+            Assert.IsTrue(string.Compare(expected, staticIO.GetOutput()) == 0);
         }
     }
 }
