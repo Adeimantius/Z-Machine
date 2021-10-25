333234549a960f21c6f44a681ac03dc4be8c777d
Jessica Mulein <jessica@mulein.com>
2021-10-24 21:44:21 -0700

    add moq

--- Build1/zmachine.Library.Tests/TestZMachineOpcodes.cs
+++ Build1/zmachine.Library.Tests/TestZMachineOpcodes.cs
@@ -1,6 +1,7 @@
 ﻿namespace zmachine.Library.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
     using System.Collections.Generic;
     using zmachine.Library.Enumerations;
     using zmachine.Library.Interfaces;
@@ -23,20 +24,19 @@
             byte pcByte = machine.pc_getByte();
         }
 
-
+        /// <summary>
+        /// Memory test. TODO: move to memory
+        /// </summary>
         [TestMethod]
         public void Test_setWord()
         {
-            IIO staticIO = new NullIO();
-            Machine machine = new Machine(
-                staticIO,
-                TestZMachine.ZorkPath,
-                new Dictionary<BreakpointType, BreakpointAction>
-                {
-                });
+            Memory testMemory = new Memory(Machine.MemorySizeByVersion[3]);
             uint address = 1234;
             ushort value = 2345;
-            machine.Memory.setWord(address: address, val: value);
+            testMemory.setWord(address: address, val: value);
+
+            var memoryCheck = testMemory.getWord(address: address);
+            Assert.AreEqual(expected: value, actual: memoryCheck);
         }
 
         [TestMethod]
@@ -49,14 +49,23 @@
                 new Dictionary<BreakpointType, BreakpointAction>
                 {
                 });
-            ushort variable = 1234;
+
             ushort value = 2345;
-            machine.setVar(variable: variable, value: value);
+            ushort[] variables = new ushort[] { 0, 0x0f, 0xff };
+            foreach (var variable in variables)
+            {
+                machine.setVar(
+                    variable: variable,
+                    value: value);
+            }
             // test all three branches of setVar
             // make sure memory setWord was called
+            // Variable number $00 refers to the top of the stack
+            //$01 to $0f mean the local variables of the current routine
+            //and $10 to $ff mean the global variables.
         }
 
-        [TestMethod]
+            [TestMethod]
         public void Test_popRoutineData()
         {
             IIO staticIO = new NullIO();
